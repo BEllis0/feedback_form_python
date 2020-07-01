eb985ba6ccea80b21dd6f504a57fa1d5d45a511b
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-01 16:31:02 -0400

    added comments

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# routes
+
+# index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# initiated server
 if __name__ == '__main__':
     app.debug = True
     app.run()
