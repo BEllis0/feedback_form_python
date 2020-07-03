574b1f6c3d377c51b265aea2d75c474628bca917
Brandon Ellis <brandonellis.email@gmail.com>
2020-07-03 08:46:37 -0400

    updates

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+# ============
+# DB CONNECTION
+# ============
+
+ENV = 'dev'
+
+# dev environment
+if ENV == 'dev':
+    app.debug = True
+    # DB CONNECTION - local
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Skiclub0@localhost/experience_rating_emails'
+# production environment
+else: 
+    app.debug = False
+    # DB CONNECTION - Heroku
+    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+# db object for querying
+db = SQLAlchemy(app)
+
+# ============
+# DB Models
+# ============
+
+
+# ============
 # routes
+# ============
 
 # index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# form submission
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
@@ -28,8 +60,10 @@ def submit():
         # serve success page
         return render_template('success.html')
 
+
+# ============
 # initiated server
+# ============
 if __name__ == '__main__':
-    app.debug = True
     app.run()
 
